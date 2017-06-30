96f92e21c0c751e87062eb5045d521be5612fe83
Maxemilian Grönblom <maxemiliang@gmail.com>
2017-07-01 00:45:43 +0300

    added vuetify

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-toolbar class="blue" light>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        title: 'Maxemilian Grönbloms Portfolio'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
