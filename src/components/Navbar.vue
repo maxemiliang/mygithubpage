a7ff6b3ceb82de78d097f1766a95394b3c50d2b4
Maxemilian Grönblom <maxemiliang@gmail.com>
2018-01-29 21:08:18 +0200

    added something

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="columns nav">
+    <div class="column is-4">
+      <h1>Maxemilian Grönbloms Portfolio</h1>
+    </div>
+    <div class="column is-8">
+      <router-link to="/">Home</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .nav {
+    padding: 1rem;
+  }
+</style>
